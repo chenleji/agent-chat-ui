6d61ac4e3b1152f5992db0b7a4745ab216baffeb
Leji Chen <chenleji@gmail.com>
2025-04-17 14:36:48 +0800

    更新对file和image的路由

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,19 @@ const nextConfig = {
     async rewrites() {
         return [
             {
+                // 所有图片管理相关服务
                 source: '/image/:path*',
-                destination: 'https://your-image-server.com/:path*', // 替换为图片服务器地址
+                destination: 'http://localhost:8000/:path*',
             },
             {
-                source: '/auth/:path*',
-                destination: 'https://your-image-server.com/:path*', // 替换认证服务器地址
+                // 所有文件管理相关服务
+                source: '/file/:path*',
+                destination: 'http://localhost:8000/:path*', 
+            },
+            {
+                // 所有用户管理相关服务
+                source: '/user/:path*',
+                destination: 'http://localhost:8000/:path*', 
             },
             {
                 // 添加对/info的代理
@@ -23,18 +30,13 @@ const nextConfig = {
                 source: '/api/:path*',
                 destination: 'http://localhost:2024/:path*', // 指向本地LangGraph服务
             },
-            {
-                // 所有用户管理相关服务
-                source: '/user/:path*',
-                destination: 'http://localhost:8000/:path*', // 指向本地用户管理服务
-            }
         ];
     },
 
     async headers() {
         return [
             {
-                source: '/(info|api|image|auth|user/:path*)',
+                source: '/(info|api|image|file|user/:path*)',
                 headers: [
                     // 其他头...
                     { key: 'Access-Control-Allow-Headers', value: 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version, X-Api-Key' },
