3d5f1f4859aaaba0d89a697830653e0474156948
chenleji <chenleji@gmail.com>
2025-04-18 23:45:07 +0800

    debug

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -126,7 +126,7 @@ export default function LoginPage() {
   };
 
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-4 py-8 -mt-35 sm:px-6 lg:px-8">
+    <div className="flex min-h-screen flex-col items-center justify-center bg-gray-50 px-4 py-8 -mt-12 sm:px-6 lg:px-8">
       {/* 系统Logo和名称 */}
       <div className="mb-10 flex flex-col items-center">
         <img src="/insure-logo.png" alt="InsureX Logo" className="h-16 w-16 mb-4" />
